1557084f3fee95b62a8dab2d94397dcef0b98f04
cathal <cathalmaguire@protonmail.com>
2018-07-12 23:09:10 +0100

    update Player to record scores in list for future analysis.

--- Player.cs
+++ Player.cs
@@ -1,25 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 namespace dartScoreboard
 {
     class Player
     {
         private string playerName;
-        private int playerScore;
-        public Player(string name, int score)
+        private int currentScore;
+        List<int> scoreList = new List<int>();
+        public Player(string name, int startScore)
         {
             playerName = name;
-            playerScore = score;
+            currentScore = startScore;
         }
 
-        public int SubstractScore(int latestScore) => playerScore - latestScore;
+        /// <summary>
+        /// Updates currentScore by substracting latestScore from currentScore
+        /// Appends latestScore to scoreList for statistic generation 
+        /// /// </summary>
+        /// <param name="latestScore">latestScore by player</param> 
+        /// <returns>Updated currentScore</returns>
+        public int SubstractScore(int latestScore) {
+            currentScore =- latestScore;
+            scoreList.Add(latestScore);
+            return currentScore;               
 
-        public Boolean checkedOut(int latestScore)
+        }
+
+        /// <summary>
+        /// Checks if player has checked out bust with latestScore
+        /// </summary>
+        /// <param name="latestScore">latestScore by player</param>
+        /// <returns>Boolean that describes whether or not the player has checked out.</returns>
+        public Boolean CheckedOut(int latestScore)
         {   
 
-            if((playerScore == 50) || (playerScore <= 40 && playerScore % 2 == 0))
+            if((currentScore == 50) || (currentScore <= 40 && currentScore % 2 == 0))
             {
-                if(playerScore - latestScore == 0)
+                if(currentScore - latestScore == 0)
                 {
                     return true;
                 }
